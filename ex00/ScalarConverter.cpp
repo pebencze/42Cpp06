aad35c1824b58e7d015ca0e4ba9d5564e062e7ab
pebencze <petra.bncz@gmail.com>
2025-02-03 15:12:35 +0100

    ex00: in progress

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -23,7 +23,7 @@ ScalarConverter & ScalarConverter::operator=(const ScalarConverter &rhs) {
 
 namespace Utils {
    int specialCases(std::string const &literal) {
-      if (literal == "+inf" || literal == "-inf" 
+      if (literal == "+inf" || literal == "-inf"
          || literal == "+inff" ||  literal == "-inff"
          || literal == "nan" || literal == "nanf") {
             std::cout << "char: impossible" << std::endl;
@@ -64,24 +64,24 @@ namespace Utils {
          std::cout << "double: " << d << std::endl;
    }
 
-   void convertToFloat(char *c, int *i, float *f, double *d, std::string const &literal) {
-         *f = std::stof(literal);
+/*    void convertToFloat(char *c, int *i, float *f, double *d, std::string const &literal) {
+         *f = atof(literal.c_str());
          *d = static_cast<double>(*f);
          *c = static_cast<char>(*f);
          *i = static_cast<int>(*f);
-   }
+   } */
    void convertToDouble(char *c, int *i, float *f, double *d, std::string const &literal) {
-         *d = std::stod(literal);
+         *d = strtod(literal.c_str(), NULL);
          *f = static_cast<float>(*d);
          *c = static_cast<char>(*d);
          *i = static_cast<int>(*d);
    }
-   void convertToInt(char *c, int *i, float *f, double *d, std::string const &literal) {
-         *i = std::stoi(literal);
+/*    void convertToInt(char *c, int *i, float *f, double *d, std::string const &literal) {
+         *i = atoi(literal.c_str());
          *d = static_cast<double>(*i);
          *c = static_cast<char>(*i);
          *f = static_cast<float>(*i);
-   }
+   } */
 }
 
 void ScalarConverter::convert(std::string const &literal) {
@@ -90,29 +90,28 @@ void ScalarConverter::convert(std::string const &literal) {
       int i = 0;
       float f = 0.0f;
       double d = 0.0;
-      
+
       if (Utils::specialCases(literal))
          return ;
-      if (literal.length() == 1 && !isdigit(literal[0])) {
+      if (literal.length() == 1) {
          c = static_cast<char>(literal[0]);
          i = static_cast<int>(c);
          d = static_cast<double>(c);
          f = static_cast<float>(c);
-         return (Utils::printCases(c, i, f, d)); 
-      } else if (literal.find("f") != std::string::npos && literal.find(".") != std::string::npos) {
+         return (Utils::printCases(c, i, f, d));
+/*       } else if (literal.find("f") != std::string::npos && literal.find(".") != std::string::npos) {
          try {
             Utils::convertToFloat(&c, &i, &f, &d, literal);
          } catch (std::exception &e) {
             Utils::convertToDouble(&c, &i, &f, &d, literal);
          }
       } else if (literal.find(".") != std::string::npos) {
-         Utils::convertToDouble(&c, &i, &f, &d, literal);
+         Utils::convertToDouble(&c, &i, &f, &d, literal); */
       } else {
-         try {
+         /* try {
             Utils::convertToInt(&c, &i, &f, &d, literal);
-         } catch (std::exception &e) {
-            Utils::convertToDouble(&c, &i, &f, &d, literal);
-         }
+         } catch (std::exception &e) { */
+         Utils::convertToDouble(&c, &i, &f, &d, literal);
       }
       Utils::printCases(c, i, f, d);
    } catch (std::invalid_argument &e) {
