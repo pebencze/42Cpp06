827e593f70b948f2f987e02cfc33872679ef5ea4
pebencze <petra.bncz@gmail.com>
2025-02-03 15:12:14 +0100

    made changes to ex01 and ex02

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -36,28 +36,28 @@ void identify(Base* p) {
         std::cout << "B" << std::endl;
     else if (dynamic_cast<C *>(p))
         std::cout << "C" << std::endl;
-    else 
+    else
         std::cout << "NULL" << std::endl;
 }
 /*It prints the actual type of the object pointed to by p: "A", "B" or "C"
- Using a pointer inside this function is forbidden. 
+ Using a pointer inside this function is forbidden.
  Downcasting -> use of dynamic_cast.*/
 void identify(Base& p) {
     try {
         (void)dynamic_cast<A &>(p);
         std::cout << "A" << std::endl;
         return ;
-    } catch (const std::bad_cast&) {} //const prevents accidental modification of the object
+    } catch (const std::exception&) {} //const prevents accidental modification of the object
     try {
         (void)dynamic_cast<B &>(p);
         std::cout << "B" << std::endl;
         return ;
-    } catch (const std::bad_cast&) {}
+    } catch (const std::exception&) {}
     try {
         (void)dynamic_cast<C &>(p);
         std::cout << "C" << std::endl;
         return ;
-    } catch (const std::bad_cast&) {
+    } catch (const std::exception&) {
         std::cout << "NULL" << std::endl;
     }
 }
